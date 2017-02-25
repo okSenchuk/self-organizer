82e3fc86dfd442dfe3c0e50e30f9c6b9824eec36
Oksana Senchuk <senchuk.oksana@gmail.com>
2017-02-25 10:56:37 +0200

    init project

---
+++ src/test/java/com/sendev/selforganizer/BaseIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.sendev.selforganizer;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class BaseIntegrationTest {
+
+    @BeforeClass
+    public static void setUp() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+    }
+}
